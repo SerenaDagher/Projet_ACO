4f0e3ecf4e9a3daf1734793eb6543e43b3af7b37
josephdoumit27 <joseph.g.tarabay@gmail.com>
2026-03-11 23:15:39 +0100

    Mpi_

--- src/parallelisation_premiere_facon/ant_simu_mpi.cpp
+++ src/parallelisation_premiere_facon/ant_simu_mpi.cpp
@@ -292,55 +292,7 @@ int main(int argc, char* argv[])
 
     if (rank == 0) {
         csv.close();
-
-        double mean_ants   = mean(stats.sum_ants, stats.count);
-        double mean_sync   = mean(stats.sum_sync, stats.count);
-        double mean_evap   = mean(stats.sum_evap, stats.count);
-        double mean_update = mean(stats.sum_update, stats.count);
-
-        double std_ants   = stddev(stats.sum_ants, stats.sum2_ants, stats.count);
-        double std_sync   = stddev(stats.sum_sync, stats.sum2_sync, stats.count);
-        double std_evap   = stddev(stats.sum_evap, stats.sum2_evap, stats.count);
-        double std_update = stddev(stats.sum_update, stats.sum2_update, stats.count);
-
-        double total_all = stats.sum_ants + stats.sum_sync + stats.sum_evap + stats.sum_update;
-
-        std::cout << "La première nourriture est arrivée au nid à l'itération "
-                  << first_food_iter << "\n\n";
-
-        std::cout << "===== RÉSULTATS VERSION MPI =====\n";
-        std::cout << "Processus                    : " << nprocs << "\n";
-        std::cout << "Nb itérations                : " << stats.count << "\n";
-        std::cout << "Quantité finale nourriture   : " << food_quantity << "\n\n";
-
-        std::cout << "--- temps_fourmis_ms ---\n";
-        std::cout << "Temps total    : " << stats.sum_ants << " ms\n";
-        std::cout << "Temps moyen    : " << mean_ants << " ms\n";
-        std::cout << "Écart-type     : " << std_ants << " ms\n";
-        std::cout << "Pourcentage    : " << (100.0 * stats.sum_ants / total_all) << " %\n\n";
-
-        std::cout << "--- temps_sync_ms ---\n";
-        std::cout << "Temps total    : " << stats.sum_sync << " ms\n";
-        std::cout << "Temps moyen    : " << mean_sync << " ms\n";
-        std::cout << "Écart-type     : " << std_sync << " ms\n";
-        std::cout << "Pourcentage    : " << (100.0 * stats.sum_sync / total_all) << " %\n\n";
-
-        std::cout << "--- temps_evap_ms ---\n";
-        std::cout << "Temps total    : " << stats.sum_evap << " ms\n";
-        std::cout << "Temps moyen    : " << mean_evap << " ms\n";
-        std::cout << "Écart-type     : " << std_evap << " ms\n";
-        std::cout << "Pourcentage    : " << (100.0 * stats.sum_evap / total_all) << " %\n\n";
-
-        std::cout << "--- temps_update_ms ---\n";
-        std::cout << "Temps total    : " << stats.sum_update << " ms\n";
-        std::cout << "Temps moyen    : " << mean_update << " ms\n";
-        std::cout << "Écart-type     : " << std_update << " ms\n";
-        std::cout << "Pourcentage    : " << (100.0 * stats.sum_update / total_all) << " %\n\n";
-
-        std::cout << "===== GLOBAL =====\n";
-        std::cout << "Temps total des colonnes analysées : " << total_all << " ms\n";
-    }
-
+    } 
     MPI_Finalize();
     return 0;
 }
